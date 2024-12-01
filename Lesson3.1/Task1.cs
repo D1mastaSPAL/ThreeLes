7a2bddf9dc28f17a300174a74f3614acaae1ae5c
D1mastaSPAL <su66071n@mail.ru>
2024-12-01 16:48:28 +0300

    first commit

---
+++ Lesson3.1/Task1.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson3._1
+{
+    internal class Task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваш возраст (полных лет): ");
+            int firstValue;
+            firstValue = Convert.ToInt32(Console.ReadLine());
+            if (firstValue >= 0 & firstValue < 120)
+            {
+                string category = firstValue switch
+                {
+                    < 2 => "Ваша возрастная группа: Младенец",
+                    >= 3 and < 12 => "Ваша возрастная группа: Ребёнок",
+                    >= 13 and < 17 => "Ваша возрастная группа: Подросток",
+                    >= 18 and < 59 => "Ваша возрастная группа: Взрослый",
+                    _ => "Ваша возрастная группа: Пожилой"
+                };
+                Console.WriteLine(category);
+            }
+            else
+            {
+                Console.WriteLine("Не удалось определить возраст :(");
+            }
+        }
+    }
+}
---
+++ Lesson3.2/Task2.cs
@@ -0,0 +1,29 @@
+﻿namespace Lesson3._2
+{
+    internal class Task2
+    {
+        static void Main(string[] args)
+        {
+            bool firstValue, secondValue;
+            Console.WriteLine("Введите значение true / false: ");
+            firstValue = Convert.ToBoolean(Console.ReadLine());
+            string textValue1 = firstValue.ToString();
+            Console.WriteLine("Введите значение true / false: ");
+            secondValue = Convert.ToBoolean(Console.ReadLine());
+            string textValue2 = secondValue.ToString();
+            bool A = (firstValue & secondValue) ? true : false;
+            bool B = (firstValue | secondValue) ? true : false;
+            bool C = (firstValue ^ secondValue) ? true : false;
+            bool D = (firstValue && secondValue) ? true : false;
+            bool E = (firstValue || secondValue) ? true : false;
+            bool F = (firstValue != secondValue) ? true : false;
+            Console.WriteLine($"{textValue1}&{textValue2} {"\t"}" +
+                              $"{textValue1}|{textValue2} {"\t"}" +
+                              $"{textValue1}^{textValue2} {"\t"}" +
+                              $"{textValue1}&&{textValue2} {"\t"}" +
+                              $"{textValue1}||{textValue2} {"\t"}" +
+                              $"{textValue1}!={textValue2} {"\n"}" +
+                             $"{A} {"\t"}{"\t"} {B} {"\t"}{"\t"} {C} {"\t"}{"\t"} {D} {"\t"}{"\t"} {E} {"\t"}{"\t"} {F}");
+        }
+    }
+}
